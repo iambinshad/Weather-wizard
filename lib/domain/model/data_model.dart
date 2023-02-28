36bdc301e92f28862387cff0acad32565c7a8860
Mohammed Binshad <binshadp999@gmail.com>
2023-03-01 00:50:12 +0530

    icon api data model add

--- lib/domain/model/data_model.dart
+++ lib/domain/model/data_model.dart
@@ -15,35 +15,39 @@ class Weather {
   final String? feelsLikeC;
   final String? uv;
   final String? pressure;
+  final String? icon;
 
-  Weather( 
-      {this.name,
-      this.country,
-      this.localtime,
-      this.lastUpdated,
-      this.tempC,
-      this.conditionText,
-      this.windKph,
-      this.cloud,
-      this.humidity,
-      this.feelsLikeC,
-      this.uv,
-      this.pressure,});
+  Weather({
+    this.name,
+    this.country,
+    this.localtime,
+    this.lastUpdated,
+    this.tempC,
+    this.conditionText,
+    this.windKph,
+    this.cloud,
+    this.humidity,
+    this.feelsLikeC,
+    this.uv,
+    this.pressure,
+    this.icon,
+  });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
-      name: json['location']['name'].toString(),
-      country: json['location']['country'].toString(),
-      localtime: json['location']['localtime'].toString(),
-      lastUpdated: json['current']['last_updated'].toString(),
-      tempC: json['current']['temp_c'].toString(),
-      conditionText: json['current']['condition']['text'].toString(),
-      windKph: json['current']['wind_kph'].toString(),
-      cloud: json['current']['cloud'].toString(),
-      humidity: json['current']['humidity'].toString(),
-      feelsLikeC: json['current']['feelslike_c'].toString(),
-      uv: json['current']['uv'].toString(),
-      pressure: json['current']['pressure_mb'].toString()
-    );
+        name: json['location']['name'].toString(),
+        country: json['location']['country'].toString(),
+        localtime: json['location']['localtime'].toString(),
+        lastUpdated: json['current']['last_updated'].toString(),
+        tempC: json['current']['temp_c'].toString(),
+        conditionText: json['current']['condition']['text'].toString(),
+        windKph: json['current']['wind_kph'].toString(),
+        cloud: json['current']['cloud'].toString(),
+        humidity: json['current']['humidity'].toString(),
+        feelsLikeC: json['current']['feelslike_c'].toString(),
+        uv: json['current']['uv'].toString(),
+        pressure: json['current']['pressure_mb'].toString(),
+        icon: json['current']['icon'].toString()
+        );
   }
 }
