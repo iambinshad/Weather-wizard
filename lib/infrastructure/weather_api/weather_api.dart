5e166a935d37861affd41065dbd3ad4857cd629a
Mohammed Binshad <binshadp999@gmail.com>
2023-03-01 12:30:47 +0530

    network error shown on diologue box

--- lib/infrastructure/weather_api/weather_api.dart
+++ lib/infrastructure/weather_api/weather_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_wizard/core/api_key/api_key.dart';
 
@@ -8,7 +9,7 @@ import '../../domain/model/data_model.dart';
 
 class WeatherApiClass {
   List<Weather> weatherNotifier = [];
-  static Future<Weather?> fetchWeatherDetails({required String searchCity}) async {
+  static Future<Weather?> fetchWeatherDetails({required String searchCity,context}) async {
     Weather data;
     try {
       var cityy = searchCity;
@@ -29,6 +30,19 @@ class WeatherApiClass {
       }
     } catch (e) {
       log("not connected to internet $e");
+      
+      // showDialog(
+      //     context: context,
+      //     builder: ((context) => AlertDialog(
+      //           content: Row(
+      //             children: const [
+      //               Text("Something went wrong..."),
+      //               SizedBox(width: 15,),
+      //               Icon(Icons
+      //                   .signal_wifi_statusbar_connected_no_internet_4_sharp)
+      //             ],
+      //           ),
+      //         )));
     }
     return null;
   }
